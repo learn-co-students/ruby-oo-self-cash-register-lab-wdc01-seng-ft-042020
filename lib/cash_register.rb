9790cea053da7872b8863eac82135debf5ad8534
epittab <ep7m@me.com>
2020-04-22 21:22:26 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
 
+class CashRegister
+
+    attr_reader :discount
+    attr_accessor :total, :items 
+
+    def initialize(discount=0)
+        @total = 0
+        @items = []
+        @discount = discount
+    end
+
+    def total
+        @total
+    end
+
+ 
+
+    def add_item(title, price, quantity = 1)
+        quantity.times do @items << [title, price, quantity] end
+        @total += (price * quantity)
+    end
+
+    def apply_discount
+        @total = @total - @total * @discount / 100.00
+        return @discount == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total.to_i}." 
+    end
+
+    def items
+        @items.map do |item| item[0] end
+    end
+
+    def void_last_transaction
+        @total -= @items[-1][1] * @items[-1][2]
+    end
+
+
+end
